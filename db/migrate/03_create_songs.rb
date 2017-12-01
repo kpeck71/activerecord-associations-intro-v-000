23ee90c004aa5d92941cdc3fd31acf20d119a000
Kristen Peck <kpeck71@gmail.com>
2017-12-01 03:02:20 +0000

    automatically backed up by learn

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,4 +1,9 @@
 class CreateSongs < ActiveRecord::Migration
   def change
+    create_table :songs |t|
+      t.string :name
+      t.integer :artist_id
+      t.integer :genre_id
+    end
   end
 end
